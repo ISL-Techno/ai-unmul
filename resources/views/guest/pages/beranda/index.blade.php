12651736f2fde4619ffc97262d74590aacf7f331
8ifarrel <farrelsirah@gmail.com>
2025-09-08 22:32:18 +0800

    feat: add hero section to beranda page

--- resources/views/guest/pages/beranda/index.blade.php
+++ resources/views/guest/pages/beranda/index.blade.php
@@ -4,4 +4,12 @@
 @endsection
 
 @section('document.body')
+<div class="hero bg-light py-5 text-center">
+    <div class="container">
+        <h1 class="display-4 mb-3">Prototype AI UNMUL</h1>
+        <img src="" alt="">
+        <p class="lead mb-4">Kasih Bagoyang</p>
+        <a href="#" class="btn btn-neutral btn-lg">Get Started</a>
+    </div>
+</div>  
 @endsection
